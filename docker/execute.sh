437f6e2a8b807ae8ea22188a77898e90aa29a89a
Julian Winter <mail@julianwinter.org>
2015-04-19 21:58:37 +0200

    added docker stuff for running jboss in a container, not finished yet -_-

---
+++ docker/execute.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+HOME=/opt/jboss
+JBOSS_HOME=$HOME/wildfly
+JBOSS_CLI=$JBOSS_HOME/bin/jboss-cli.sh
+JBOSS_MODE=${1:-"standalone"}
+JBOSS_CONFIG=${2:-"$JBOSS_MODE.xml"}
+
+function wait_for_server() {
+  until `$JBOSS_CLI -c "ls /deployment" &> /opt/jboss/log`; do
+    sleep 1
+  done
+}
+
+echo "=> Starting WildFly server"
+$JBOSS_HOME/bin/$JBOSS_MODE.sh -c $JBOSS_CONFIG > /opt/jboss/log &
+
+echo "=> Waiting for the server to boot"
+wait_for_server
+
+echo "=> Executing the commands"
+$JBOSS_CLI -c --file=$HOME/config/commands.cli
+
+sleep 5
+
+echo "=> Shutting down WildFly"
+if [ "$JBOSS_MODE" = "standalone" ]; then
+  $JBOSS_CLI -c ":shutdown"
+else
+  $JBOSS_CLI -c "/host=*:shutdown"
+fi
