804e74d8b249c32c307c52d7fbe6b3e0ec834c71
Julian Winter <julian.winter@k-tc.de>
2015-09-22 18:53:42 +0200

    foo

--- docker/init.sh
+++ docker/init.sh
@@ -5,3 +5,6 @@ docker build --tag=kaojo/wildfly ./wildfly-9.1/
 docker build --tag=kaojo/postgres ./postgres-9.4/
 
 docker build --tag=kaojo/jboss-log ./jbossLog/
+
+# build dev images
+docker build --tag=kaojo/wildfly-debug ./dev/wildfly-debug/
